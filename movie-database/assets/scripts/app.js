24297e177d0c156e88dff1971fb8b4621917d006
John Koen <jkoen@smartronix.com>
2021-09-20 10:16:15 -0400

    Initial commit

---
+++ movie-database/assets/scripts/app.js
@@ -0,0 +1,141 @@
+const addMovieModal = document.getElementById("add-modal");
+const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+const entryTextSection = document.getElementById("entry-text");
+const deleteMovieModal = document.getElementById("delete-modal");
+
+const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+    <div class= "movie-element__image">
+    <img src="${imageUrl} alt="${title}>
+    </div>
+    <div class= "movie-element__info">
+    <h2>${title}</h2>
+    <p>${rating}/5 stars</p>
+    `;
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
+  const listRoot = document.getElementById("movie-list");
+  listRoot.append(newMovieElement);
+};
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const closeMovieModal = () => {
+  addMovieModal.classList.remove("visible");
+  toggleBackdrop();
+};
+
+const closeMovieDeletionModal = () => {
+  toggleBackdrop();
+  deleteMovieModal.classList.remove("visible");
+  toggleBackdrop();
+};
+
+const showMovieModal = () => {
+  addMovieModal.classList.add("visible");
+  toggleBackdrop();
+};
+
+const clearMovieInput = () => {
+  for (const usrInput of userInputs) {
+    usrInput.value = "";
+  }
+};
+const backdropClickHandler = () => {
+  closeMovieModal();
+  closeMovieDeletionModal();
+  clearMovieInput();
+};
+
+const cancelAddMovieHandler = () => {
+  closeMovieModal();
+  clearMovieInput();
+};
+
+const deleteMovie = (movieId) => {
+  let movieIndex = 0;
+
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById("movie-list");
+  listRoot.children[movieIndex].remove();
+};
+
+const deleteMovieHandler = (movieId) => {
+  const cancelDeletionButton = deleteMovieModal.querySelector(".btn--passive");
+  const confirmDeletionButton = deleteMovieModal.querySelector(".btn--danger");
+
+  cancelDeletionButton.addEventListener("click", closeMovieDeletionModal);
+  confirmDeletionButton.addEventListener(
+    "click",
+    deleteMovie.bind(null, movieId)
+  );
+  deleteMovieModal.classList.add("visible");
+  toggleBackdrop();
+  closeMovieDeletionModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5).");
+    return;
+  }
+
+  const newMovie = {
+    id: Math.random().toString(),
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  closeMovieModal();
+  toggleBackdrop();
+  clearMovieInput();
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
+  updateUI();
+};
+
+startAddMovieButton.addEventListener("click", showMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
